51f68d49b0d251dc2ca57467a0e5b37086c055c1
Shikha Rani <srani009@gmail.com>
2022-02-10 00:07:14 +0530

    table

---
+++ table.js
@@ -0,0 +1,11 @@
+let a=2;
+console.log("2*1="+a*1)
+console.log("2*2="+a*2)
+console.log("2*3="+a*3)
+console.log("2*4="+a*4)
+console.log("2*5="+a*5)
+console.log("2*6="+a*6)
+console.log("2*7="+a*7)
+console.log("2*8="+a*8)
+console.log("2*9="+a*9)
+console.log("2*10="+a*10)
